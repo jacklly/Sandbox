c231a3643eb9e2526b11a9ec2ed7d7f4f6835423
jckkl <jackkelly01@my.jcu.edu.au>
2022-09-23 11:59:20 +1000

    Gave functions to score file

--- Sandbox/score.py
+++ Sandbox/score.py
@@ -1,18 +1,19 @@
 def main():
-    score = float(input("Enter score: "))
+    score = 0
+    score = score_getter(score)
+    if score > 100 or score < 0:
+        print("Invalid score")
+    elif score >= 90:
+        print("Excellent score.")
+    elif score >= 50:
+        print("Passable score")
+    else:
+        print("Bad score")
 
 
-def random_score(main):
-
+def score_getter(score):
+    score = int(input("Enter score: "))
+    return score
 
 
 main()
-
-if score > 100 or score < 0:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent score.")
-elif score >= 50:
-    print("Passable score")
-else:
-    print("Bad score")
