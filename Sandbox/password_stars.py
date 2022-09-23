12958c6e3477b6db449b4ab567ec2dec555c6c7a
jckkl <jackkelly01@my.jcu.edu.au>
2022-09-23 11:25:14 +1000

    Added new 'def' code

--- Sandbox/password_stars.py
+++ Sandbox/password_stars.py
@@ -1,9 +1,17 @@
-Psw = "0"
-Psw = input("What would you like to set your password as? (Min. 5 characters)")
+def main():
+    """Password checking"""
+    Psw = "0"
+    Psw = get_password(Psw)
 
-while len(Psw) < 5:
-    print("Please enter a longer password!")
+    PswLen = len(Psw)
+    print("Your password is: ", PswLen * "*")
+
+def get_password(Psw):
     Psw = input("What would you like to set your password as? (Min. 5 characters)")
+    while len(Psw) < 5:
+        print("Please enter a longer password!")
+        Psw = input("What would you like to set your password as? (Min. 5 characters)")
+    return Psw
 
-PswLen = len(Psw)
-print("Your password is: ", PswLen * "*")
+
+main()
