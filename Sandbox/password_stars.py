1f901a30820ddb0ab81ef982a02a55ae6c512cc0
jckkl <jackkelly01@my.jcu.edu.au>
2022-09-23 11:01:02 +1000

    A meaningful message

--- Sandbox/password_stars.py
+++ Sandbox/password_stars.py
@@ -1,9 +1,9 @@
 Psw = "0"
+Psw = input("What would you like to set your password as? (Min. 5 characters)")
 
 while len(Psw) < 5:
+    print("Please enter a longer password!")
     Psw = input("What would you like to set your password as? (Min. 5 characters)")
-    if len(Psw) < 5:
-        print("Please enter a longer password!")
 
 PswLen = len(Psw)
 print("Your password is: ", PswLen * "*")
